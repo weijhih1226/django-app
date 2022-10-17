f301da95fbb1c0a24bb802bbf0d96585c43123c4
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-10-17 18:26:03 +0800

    新增TWindy網頁

--- twindy/gallery/views.py
+++ twindy/gallery/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import requests
+import json
+
+tafis_api = 'https://tafis2.cwb.gov.tw/tafis/api/'
+token = 'iaU5doZ8-xHOK7sonSHAff-Zc8E'
+response_ca = requests.get(url=tafis_api+'cyclone/active/', headers={"Authorization": f"Token {token}"})
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, Django!")
+    return render(request , 'index.html')
+
+def home(request):
+    return HttpResponse(response_ca.text)
     
--- twindy/twindy/views.py
+++ twindy/twindy/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, Django home!")
+    return HttpResponse("Hello, TWindy Home!")
     
