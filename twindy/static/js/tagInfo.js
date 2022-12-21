6a6efd535e3e6afa1b22e8597e78a9e60ecf2fd9
weijhih1226 <weijhih1226@yahoo.com.tw>
2022-12-21 18:31:28 +0800

    tagName.js更名

---
+++ twindy/static/js/tagInfo.js
@@ -0,0 +1,47 @@
+export const MAIN = 'main'
+export const CONTENT = 'content'
+export const SIDEMENU = 'sidemenu'
+export const CLS_CONTENT = '.' + CONTENT
+export const CLS_SIDEMENU = '.' + SIDEMENU
+
+export const TS_CTN = 'ts-ctn'
+export const TS_BODY = 'ts-body'
+export const TS_TRACK = 'ts-track'
+export const TS_BAR = 'ts-bar'
+export const TS_ANCHOR = 'ts-anchor'
+export const TS_POINTER = 'ts-pointer'
+export const TS_POINTER_TAG = 'ts-pointer-tag'
+export const TS_BAR_POINTER = 'ts-bar-pointer'
+export const TS_BAR_TAG = 'ts-bar-tag'
+export const TS_CTL = 'ts-ctl'
+export const TS_CTL_PLAY = 'ts-ctl-play'
+export const TS_CTL_FORWARD = 'ts-ctl-forward'
+export const TS_CTL_REWIND = 'ts-ctl-rewind'
+export const CLS_TS_CTL_PLAY = 'icofont-play-alt-1 icofont-2x'
+export const CLS_TS_CTL_FORWARD = 'icofont-forward icofont-2x'
+export const CLS_TS_CTL_REWIND = 'icofont-rewind icofont-2x'
+export const CLS_TS_TICK = 'tick'
+export const CLS_TS_TICK_1HR = 'tick-1hr'
+export const CLS_TS_TICK_30MIN = 'tick-30min'
+export const CLS_TS_TICK_10MIN = 'tick-10min'
+export const CLS_TS_TICK_INFO = 'tick-info'
+export const CLS_TS_BODY = 'ts-body'
+export const TS = 'timeslider'
+export const ID_TS_CTN = '#' + TS_CTN
+export const ID_TS_POINTER = '#' + TS_POINTER
+export const CLS_TS = '.' + TS
+
+export const BG_ZOOM = 'bg-zoom'
+export const LK_ZOOM = 'lk-zoom'
+export const IMG_ZOOM = 'img-zoom'
+export const GALLERY_IMGS = '.content>div>.pic>a>img'
+export const ID_BG_ZOOM = '#' + BG_ZOOM
+
+
+export const BTN_SIDEMENU = 'btn-sidemenu'
+export const COLLAPSE = 'collapse'
+export const ID_BTN_SIDEMENU = '#' + BTN_SIDEMENU
+export const CLS_COLLAPSE = '.' + COLLAPSE
+
+export const MENU_LIST_TITLE = '#menu>li>a'
+export const CLS_BTN_GOTOP = 'a.btn-goTop'
